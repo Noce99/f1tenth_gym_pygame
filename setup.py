6163dcaa573d3a082ba060e90d9b010dfb7e414c
Noce99 <enrico.mannocci99@gmail.com>
2024-06-30 21:09:53 +0200

    Working setup.py with python 3.8.19 and pip 23.0.1!

--- setup.py
+++ setup.py
@@ -7,13 +7,21 @@ setup(name='f110_gym',
       url='https://f1tenth.org',
       package_dir={'': 'gym'},
       install_requires=[
+          'setuptools==56.0.0',
+          'opencv-python==4.10.0.84',
+          'cloudpickle==1.6.0',
+          'future==1.0.0',
           'gym==0.19.0',
-          'numpy<=1.22.0,>=1.18.0',
-          'Pillow>=9.0.1',
-          'scipy>=1.7.3',
-          'numba>=0.55.2',
-          'pyyaml>=5.3.1',
-          'pyglet<1.5',
-          'pyopengl'
+          'importlib_metadata==8.0.0',
+          'llvmlite==0.41.1',
+          'numba==0.58.1',
+          'numpy==1.22.0',
+          'pillow==10.3.0',
+          'pygame==2.6.0',
+          'pyglet==1.4.11',
+          'PyOpenGL==3.1.7',
+          'PyYAML==6.0.1',
+          'scipy==1.10.1',
+          'zipp==3.19.2'
       ]
       )
